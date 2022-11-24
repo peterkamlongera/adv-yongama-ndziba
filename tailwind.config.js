6d204e09e98eec7f5305144bf002e06ef696fa5d
peterkamlongera <peterkamlongera@gmail.com>
2022-11-24 17:25:14 +0200

    Project initialized

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+// /** @type {import('tailwindcss').Config} **/
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    fontFamily: {
+      display: ["Petrona"],
+      body: ["Martel"]
+    },
+    gridTemplateRows: {
+      pancake: "auto 1fr auto"
+    },
+    fontSize: {
+      'ms': '.5rem',
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'tiny': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '4.5rem',
+      '8xl': '6rem',
+      '9xl': '8rem',
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'color-1': '#212738',
+      'color-2': '#7A7978',
+      'color-3': '#EDF2EF',
+      'black': '#000000',
+      'color-5': '#353D03',
+    },
+    backgroundImage: {
+      'pattern-1': "url('../assets/svg/confetti-doodles.svg')"
+    },
+    extend: {
+  plugins: [],
+  }
+}
+}
